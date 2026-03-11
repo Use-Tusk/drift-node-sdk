162a52472a49f1499382bb187c602220864c2336
Sohil Kshirsagar <sohil@usetusk.ai>
2026-03-11 14:18:07 -0700

    fix: update e2e test next.config imports to /next subpath

--- src/instrumentation/libraries/nextjs/e2e-tests/cjs-nextjs/next.config.js
+++ src/instrumentation/libraries/nextjs/e2e-tests/cjs-nextjs/next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const { withTuskDrift } = require("@use-tusk/drift-node-sdk");
+const { withTuskDrift } = require("@use-tusk/drift-node-sdk/next");
 
 module.exports = withTuskDrift(
   {},
